fdf986bd8eec5b7783c293f130ac2d7421905b8d
Shree Nidhi <shreenidhi.tvk@gmail.com>
2017-09-10 22:40:30 +0530

    Adding : folder utility module

---
+++ modules/__init__.py
@@ -0,0 +1,16 @@
+
+
+__author__ = "Shreenidhi Ramachandra"
+__copyright__ = "GNU Public license"
+__credits__ = ["Rob Knight", "Peter Maxwell", "Gavin Huttley",
+                    "Matthew Wakefield"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Shreenidhi Ramachandra"
+__email__ = "Shreenidhi.tvk@gmail.com"
+__status__ = "Training"
+
+
+__all__=["folderutility"]
+
+from folderutility import FolderUtility
---
+++ modules/folderutility.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+ # -*- coding: utf-8 -*-
+
+"""
+Author  : Shreenidhi Ramachandra
+Date    : 11 September,2017
+Email   : Shreenidhi.tvk@gmail.com
+Usage   : check for the folder existence and create if necessary
+"""
+
+class FolderUtility(object):
+    """docstring for FolderUtility.
+        Handels folder creation
+    """
+    def __init__(self, arg):
+        super(FolderUtility, self).__init__()
+        self.arg = arg
