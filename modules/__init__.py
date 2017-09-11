1a5ebb2bfc2b93460b7d361539d721581b653c92
Shreenidhitvk <shreenidhi.tvk@gmail.com>
2017-09-11 22:19:33 +0530

    Update : Header infromation

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,14 +1,21 @@
+#!/usr/bin/python
+ # -*- coding: utf-8 -*-
 
+"""
+    Author  : Shreenidhi Ramachandra
+    Date    : 11 September,2017
+    Email   : Shreenidhi.tvk@gmail.com
+    Usage   : helps with import stuff
+"""
 
-__author__ = "Shreenidhi Ramachandra"
-__copyright__ = "GNU Public license"
-__credits__ = ["Rob Knight", "Peter Maxwell", "Gavin Huttley",
-                    "Matthew Wakefield"]
-__license__ = "GPL"
-__version__ = "1.0.1"
-__maintainer__ = "Shreenidhi Ramachandra"
-__email__ = "Shreenidhi.tvk@gmail.com"
-__status__ = "Training"
+__author__          = "Shreenidhi Ramachandra"
+__copyright__       = "GNU Public license"
+__credits__         = [""]
+__license__         = "GPL"
+__version__         = "0.0.1"
+__maintainer__      = "Shreenidhi Ramachandra"
+__email__           = "Shreenidhi.tvk@gmail.com"
+__status__          = "Training"
 
 
 __all__=["folderutility"]
--- modules/folderutility.py
+++ modules/folderutility.py
@@ -2,11 +2,13 @@
  # -*- coding: utf-8 -*-
 
 """
-Author  : Shreenidhi Ramachandra
-Date    : 11 September,2017
-Email   : Shreenidhi.tvk@gmail.com
-Usage   : check for the folder existence and create if necessary
+    Author  : Shreenidhi Ramachandra
+    Date    : 11 September,2017
+    Email   : Shreenidhi.tvk@gmail.com
+    Usage   : check for the folder existence and create if necessary
 """
+
+import os
 
 class FolderUtility(object):
     """docstring for FolderUtility.
@@ -15,3 +17,16 @@ class FolderUtility(object):
     def __init__(self, arg):
         super(FolderUtility, self).__init__()
         self.arg = arg
+
+    def is_available(self,folder_name):
+        if os.path.exist(folder_name):
+            return pass
+
+    def create_folder(self,folder_name):
+        if self.is_available(folder_name):
+            print "Info     : Folder "+folder_name+" already exists!"
+        else:
+            try:
+                pass
+            except Exception as e:
+                raise
